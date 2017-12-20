641a5d9bb9c8a54c4137e6a278aadd2b3781b609
Damian <veraxys@gmail.com>
2017-12-20 11:12:43 +0100

    niewiem

---
+++ employee.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+class Employee{
+	string name;
+	string position;
+	int age;
+
+
+	Employee(string name2="Damian",string position2="salesman", int age2=20)
+	{
+		name=name2;
+		position=position2;
+		age=age2;
+	}
+
+
+};
